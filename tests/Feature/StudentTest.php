8f2b020ad7a7465ef81d27f330cba45e10a766e5
saeid <sa3eid.dev@gmail.com>
2022-05-06 21:24:38 +0200

    updating tests

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -14,32 +14,29 @@ class StudentTest extends TestCase
      *
      * @return void
      */
-    public function test_store_student_request()
-    {
-        $facker= Factory::create();
-        $school= \App\Models\School::first();
-    Session::start();
-    $response = $this->call('POST', 'api/students/join', array(
-        'name'=>$facker->name(),
-        'email'=> time().$facker->email,
-        'school_id'=>$school->id,
-        'password'=>'123456'
-    ));
-    $this->assertEquals(200, $response->getStatusCode());
-    }
-    function test_get_students(){
-                $school= \App\Models\School::first();
-$response = $this->call('POST', 'api/login', array(
-        
-        'email'=> 'admin@backend.com',
-       
-        'password'=>'12345678'
-    ));
-$access_token=$response->getData()->data->access_token;
-$response= $this->withHeaders([
-            'Authorization' => 'Bearer '. $access_token,
-        ])->json('GET','api/students/get', array('school_id'=>$school->id));
-    $this->assertEquals(200, $response->getStatusCode());
-
-    }
+    // public function test_store_student_request()
+    // {
+    //     $facker= Factory::create();
+    //     $school= \App\Models\School::first();
+    //     Session::start();
+    //     $response = $this->call('POST', 'api/students/join', array(
+    //         'name'=>$facker->name(),
+    //         'email'=> time().$facker->email,
+    //         'school_id'=>$school->id,
+    //         'password'=>'123456'
+    //     ));
+    //     $this->assertEquals(200, $response->getStatusCode());
+    // }
+    // function test_get_students(){
+    //     $school= \App\Models\School::first();
+    //     $response = $this->call('POST', 'api/login', array( 
+    //         'email'=> 'admin@backend.com',
+    //         'password'=>'12345678'
+    //     ));
+    //     $access_token=$response->getData()->data->access_token;
+    //     $response= $this->withHeaders([
+    //         'Authorization' => 'Bearer '. $access_token,
+    //     ])->json('GET','api/students/get', array('school_id'=>$school->id));
+    //     $this->assertEquals(200, $response->getStatusCode());
+    // }
 }

42f3d97b66e1a3bb0b1af0e62fc6c3bcf96da100
saeid <sa3eid.dev@gmail.com>
2022-05-06 21:37:37 +0200

    updated feature test file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+
 class ExampleTest extends TestCase
 {
     /**
@@ -15,7 +16,6 @@ class ExampleTest extends TestCase
     public function test_example()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
